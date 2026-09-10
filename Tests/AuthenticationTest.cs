51d07d901940f98c442ab850230fa3ac59446e39
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-11 01:12:40 +0800

    Added Test Folder to contain all Test Scripts. Added AuthenticationTest class to handle all test regarding Login page

--- Tests/AuthenticationTest.cs
+++ Tests/AuthenticationTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MyTestAutomationSeleniumCSharp.Custom;
+using MyTestAutomationSeleniumCSharp.Pages;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,23 +12,33 @@ namespace MyTestAutomationSeleniumCSharp.Tests
     public class AuthenticationTest
     {
 
+        #pragma warning disable CA1859
+        #pragma warning disable NUnit1032
+        private IWebDriver _driver;
+
         [SetUp]
         public void SetUp()
         {
-
+            _driver = new ChromeDriver();    
         }
 
         [Test]
-        public void Test1()
+        public void TC01_VerifyUserCanAcessLoginPage()
         {
+            LandingPage landingPage = new LandingPage(_driver);
+            LoginPage loginPage = new LoginPage(_driver);
 
+            landingPage.NavigateToLandingPage();
+            landingPage.ClickSignInBtn();
+
+            Assert.That(loginPage.CheckLoginPageAccessible(), "Test Failed - Accessing Login Page Not Successful");
         }
 
 
         [TearDown]
         public void TearDown()
         {
-
+            _driver.Quit();
         }
 
     }
