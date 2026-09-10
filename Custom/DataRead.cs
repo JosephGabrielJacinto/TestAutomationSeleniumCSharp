abd378fcae36d06fc76a84261223f1128afda4bd
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-11 01:08:40 +0800

    Added class to handle json deserialization

---
+++ Custom/DataRead.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Testing.Platform.Configurations;
+using MyTestAutomationSeleniumCSharp.DataModel;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace MyTestAutomationSeleniumCSharp.Custom
+{
+    public class DataRead
+    {
+        public static Account GetUserAccount()
+        {
+            string dataFromJson = File.ReadAllText(@"C:\Users\joseph jacinto\source\repos\MyTestAutomationSeleniumCSharp\data\Account.json");
+
+            var userAccount = JsonSerializer.Deserialize<Account>(dataFromJson);
+
+            return userAccount;
+        }
+
+
+    }
+}
