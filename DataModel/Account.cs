4f4a9bcb03cd219888d6de8a802a9a6bea07276a
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-11 01:09:35 +0800

    Added Account class model for json encapsulation for the account data from json file

---
+++ DataModel/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.DataModel
+{
+    public class Account
+    {
+        public string ValidUsername { get; set; }
+        public string ValidPassword { get; set; }
+    }
+}
