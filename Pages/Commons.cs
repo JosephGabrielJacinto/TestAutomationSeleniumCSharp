8303497bdd4de1e0a7b0eaeb351a61189136eb91
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-10 21:07:55 +0800

    Added Common class for common headers

---
+++ Pages/Commons.cs
@@ -0,0 +1,70 @@
+﻿using MyTestAutomationSeleniumCSharp.Custom;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.Pages
+{
+    public class Commons
+    {
+        private IWebDriver _driver;
+
+        public Commons(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+
+
+        //**Page Elements for Common**//
+        IWebElement homeLink => this._driver.FindElement(By.PartialLinkText("Home"));
+        
+        IWebElement employeesLink => this._driver.FindElement(By.PartialLinkText("Employees"));
+        
+        IWebElement dashboardLink => this._driver.FindElement(By.PartialLinkText("Dashboard"));
+        
+        IWebElement aboutLink => this._driver.FindElement(By.PartialLinkText("About"));
+        
+        IWebElement registerLink => this._driver.FindElement(By.LinkText("Register"));
+        
+        IWebElement loginLink => this._driver.FindElement(By.LinkText("Login"));
+
+
+        //**Page Actions / Methods for Common Page**//
+
+        //Method to click Home Link
+        public void ClickHomeLink()
+        {
+            Helper.ClickElement(homeLink);
+        }
+
+
+        //Method to click Employee Link
+        public void ClickEmployeeLink()
+        {
+            Helper.ClickElement(employeesLink);
+        }
+
+
+        //Method to click Dashboard Link
+        public void ClickDashboardLink()
+        {
+            Helper.ClickElement(dashboardLink);
+        }
+
+
+        //Method to click About Link
+        public void ClickAboutLink()
+        {
+            Helper.ClickElement(aboutLink);
+        }
+
+
+        //Method to click Register Link
+        public void ClickRegisterLink()
+        {
+            Helper.ClickElement(registerLink);
+        }
+
+    }
+}
