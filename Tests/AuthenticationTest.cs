fda3f2c220679f6e65b070998222fa93bf1dd5d4
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-10 21:51:58 +0800

    Added class file for Authentication Login Tests

---
+++ Tests/AuthenticationTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.Tests
+{
+    public class AuthenticationTest
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+    }
+}
