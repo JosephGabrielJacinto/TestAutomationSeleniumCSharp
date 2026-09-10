17faaae6a9026c6452444231cc794e2a61f47e66
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-10 17:54:05 +0800

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace MyTestAutomationSeleniumCSharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
