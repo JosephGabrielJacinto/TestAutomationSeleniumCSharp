80472a0163a86e02d08e2e76abf838f60cae13a8
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-15 14:39:51 +0800

    Added Employee Class to Page Object Model folder

---
+++ Pages/EmployeePage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.Pages
+{
+    public class EmployeePage
+    {
+        private IWebDriver _driver;
+
+
+        public EmployeePage(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+
+
+        //**Error Messages for Employee Page**//
+
+
+        //**Page Elements for Employee Page**//
+        IWebElement addNewEmpBtn => this._driver.FindElement(By.LinkText("+ New Employee"));
+        IWebElement empTable => this._driver.FindElement(By.CssSelector("div.employee-table-card > table"));
+
+
+
+        //**Methods for Emplyee Page**//
+        public bool CheckIfEmployeePageIsAccesible()
+        {
+            try
+            {
+                return (addNewEmpBtn.Displayed && empTable.Displayed) ? true : false;
+            }
+            catch(NoSuchElementException e)
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
