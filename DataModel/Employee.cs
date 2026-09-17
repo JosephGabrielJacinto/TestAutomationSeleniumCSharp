cfef84ea95ff54097d100af54b84b8071bd545ac
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-17 13:37:40 +0800

    Created Employee class for data encapsulation

---
+++ DataModel/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.DataModel
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set;  }
+        public int Duration { get; set; }
+        public double Salary { get; set; }
+        public int Grade { get; set; }
+        public string Email { get; set; }
+
+    }
+}
