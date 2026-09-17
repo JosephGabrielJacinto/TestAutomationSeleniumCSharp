9e0ccaab96edebbc15694f9aee08ec4d005db00c
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-17 13:38:30 +0800

    Added Delete Employee Page

---
+++ Pages/DeleteEmployeePage.cs
@@ -0,0 +1,32 @@
+﻿using MyTestAutomationSeleniumCSharp.Custom;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.Pages
+{
+    public class DeleteEmployeePage
+    {
+
+        private IWebDriver _driver;
+
+        public DeleteEmployeePage(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+
+
+        IWebElement deleteBtn => this._driver.FindElement(By.ClassName("btn-danger"));
+
+        IWebElement cancelBtn => this._driver.FindElement(By.LinkText("Cancel"));
+
+
+        public void ConfirmDeleteEmployee()
+        {
+            Helper.ClickElement(deleteBtn);
+        }
+
+
+    }
+}
