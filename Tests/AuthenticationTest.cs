2566e42e3ff12c651de23d9648a2a92a336dd732
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-22 13:45:19 +0800

    Updated test to use try catch for error handling

--- Tests/AuthenticationTest.cs
+++ Tests/AuthenticationTest.cs
@@ -10,13 +10,21 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user can access the Login Page by clicking the Sign In button")]
         public void TC01_VerifyUserCanAcessLoginPageBySignInBtn()
         {
-            LandingPage landingPage = new LandingPage(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
+            try
+            {
+                LandingPage landingPage = new LandingPage(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
 
-            landingPage.NavigateToLandingPage();
-            landingPage.ClickSignInBtn();
+                landingPage.NavigateToLandingPage();
+                landingPage.ClickSignInBtn();
 
-            Assert.That(loginPage.CheckLoginPageAccessible(), LoginPage._errMsgPageNotAccessible);
+                Assert.That(loginPage.CheckLoginPageAccessible(), LoginPage._errMsgPageNotAccessible);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Test Failed - {e.Message}");
+            }
+
         }
 
 
@@ -24,14 +32,22 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user can access the Login Page by clicking the Login from the header")]
         public void TC02_VerifyUserCanAcessLoginPageByLoginBtn()
         {
-            LandingPage landingPage = new LandingPage(this.driver);
-            Commons commons = new Commons(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
+            try
+            {
+                LandingPage landingPage = new LandingPage(this.driver);
+                Commons commons = new Commons(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
 
-            landingPage.NavigateToLandingPage();
-            commons.ClickLoginLink();
+                landingPage.NavigateToLandingPage();
+                commons.ClickLoginLink();
 
-            Assert.That(loginPage.CheckLoginPageAccessible(), LoginPage._errMsgPageNotAccessible);
+                Assert.That(loginPage.CheckLoginPageAccessible(), LoginPage._errMsgPageNotAccessible);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Test Failed - {e.Message}");
+            }
+
         }
 
 
@@ -40,23 +56,31 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user can successfully Log in using valid username and valid password")]
         public void TC03_VerifyLoginByValidUsernameAndValidPassword()
         {
-            Commons commons = new Commons(this.driver);
-            LandingPage landingPage = new LandingPage(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
-
-            landingPage.NavigateToLandingPage();
-
-            commons.ClickLoginLink();
-
-            if (loginPage.CheckLoginPageAccessible())
+            try
             {
-                loginPage.Login(DataRead.GetUserAccount().ValidUsername, DataRead.GetUserAccount().ValidPassword);
-                Assert.That(loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginNotSuccesful);
+                Commons commons = new Commons(this.driver);
+                LandingPage landingPage = new LandingPage(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
+
+                landingPage.NavigateToLandingPage();
+
+                commons.ClickLoginLink();
+
+                if (loginPage.CheckLoginPageAccessible())
+                {
+                    loginPage.Login(DataRead.GetUserAccount().ValidUsername, DataRead.GetUserAccount().ValidPassword);
+                    Assert.That(loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginNotSuccesful);
+                }
+                else
+                {
+                    Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                }
             }
-            else
+            catch (Exception e)
             {
-                Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                Assert.Fail($"Test Failed - {e.Message}");
             }
+
         }
 
 
@@ -65,21 +89,29 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user will not be able to Log in using valid username and invalid password")]
         public void TC04_VerifyLoginByValidUsernameAndInvalidPassword()
         {
-            Commons commons = new Commons(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
-
-            loginPage.NavigateToLoginPage();
-
-            if (loginPage.CheckLoginPageAccessible())
+            try
             {
-                loginPage.Login(DataRead.GetUserAccount().ValidUsername, DataRead.GetUserAccount().InvalidPassword);
-                Assert.That(loginPage.CheckErrorMsgIfVisible(), LoginPage._errMsgNotDisplayed);
-                Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidPassword);
+                Commons commons = new Commons(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
+
+                loginPage.NavigateToLoginPage();
+
+                if (loginPage.CheckLoginPageAccessible())
+                {
+                    loginPage.Login(DataRead.GetUserAccount().ValidUsername, DataRead.GetUserAccount().InvalidPassword);
+                    Assert.That(loginPage.CheckErrorMsgIfVisible(), LoginPage._errMsgNotDisplayed);
+                    Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidPassword);
+                }
+                else
+                {
+                    Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                }
             }
-            else
+            catch (Exception e)
             {
-                Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                Assert.Fail($"Test Failed - {e.Message}");
             }
+
         }
 
 
@@ -88,21 +120,29 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user will not be able to Log in using invalid username and valid password")]
         public void TC05_VerifyLoginByInvalidUsernameAndValidPassword()
         {
-            Commons commons = new Commons(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
-
-            loginPage.NavigateToLoginPage();
-
-            if (loginPage.CheckLoginPageAccessible())
+            try
             {
-                loginPage.Login(DataRead.GetUserAccount().InvalidUsername, DataRead.GetUserAccount().ValidPassword);
-                Assert.That(loginPage.CheckErrorMsgIfVisible());
-                Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidUsername);
+                Commons commons = new Commons(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
+
+                loginPage.NavigateToLoginPage();
+
+                if (loginPage.CheckLoginPageAccessible())
+                {
+                    loginPage.Login(DataRead.GetUserAccount().InvalidUsername, DataRead.GetUserAccount().ValidPassword);
+                    Assert.That(loginPage.CheckErrorMsgIfVisible());
+                    Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidUsername);
+                }
+                else
+                {
+                    Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                }
             }
-            else
+            catch (Exception e)
             {
-                Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                Assert.Fail($"Test Failed - {e.Message}");
             }
+
         }
 
 
@@ -111,21 +151,29 @@ namespace MyTestAutomationSeleniumCSharp.Tests
         [Description("Verifies that a user will not be able to Log in using invalid username and invalid password")]
         public void TC06_VerifyLoginByInvalidUsernameAndInvalidPassword()
         {
-            Commons commmonss = new Commons(this.driver);
-            LoginPage loginPage = new LoginPage(this.driver);
-
-            loginPage.NavigateToLoginPage();
-
-            if (loginPage.CheckLoginPageAccessible())
+            try
             {
-                loginPage.Login(DataRead.GetUserAccount().InvalidUsername, DataRead.GetUserAccount().InvalidPassword);
-                Assert.That(loginPage.CheckErrorMsgIfVisible());
-                Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidCredentials);
+                Commons commmonss = new Commons(this.driver);
+                LoginPage loginPage = new LoginPage(this.driver);
+
+                loginPage.NavigateToLoginPage();
+
+                if (loginPage.CheckLoginPageAccessible())
+                {
+                    loginPage.Login(DataRead.GetUserAccount().InvalidUsername, DataRead.GetUserAccount().InvalidPassword);
+                    Assert.That(loginPage.CheckErrorMsgIfVisible());
+                    Assert.That(!loginPage.CheckUserIsLoggedIn(), LoginPage._errMsgLoginSuccessfulInvalidCredentials);
+                }
+                else
+                {
+                    Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                }
             }
-            else
+            catch (Exception e)
             {
-                Assert.Fail(LoginPage._errMsgPageNotAccessible);
+                Assert.Fail($"Test Failed - {e.Message}");
             }
+
         }
 
     }
