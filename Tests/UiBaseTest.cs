d0c55078e3e35c5f671dee96ac2e2096d34b8263
JosephGabrielJacinto <josephjacinto1017@gmail.com>
2026-09-22 13:38:23 +0800

    added base test that has teardown method that takes screenshot whenever there is assert failure

---
+++ Tests/UiBaseTest.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.BrowsingContext;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomationSeleniumCSharp.Tests
+{
+    public class UiBaseTest
+    {
+
+        #pragma warning disable CA1859
+        #pragma warning disable NUnit1032
+        protected IWebDriver driver;
+        protected TestContext textContext;
+
+
+        [SetUp]
+        protected void Setup()
+        {
+            this.driver = new ChromeDriver();
+            this.textContext = TestContext.CurrentContext;
+        }
+
+        
+        [TearDown]
+        protected void TearDown()
+        {
+            //Takes Screeenshot of Test that failed its assertion
+            if (textContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                try {
+
+                    string ssDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Screenshots");
+                    Directory.CreateDirectory(ssDir);
+
+                    string fileName = $"{textContext.Test.Name}.png";
+                    string filePath = Path.Combine(ssDir, fileName);
+
+                    var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                    screenshot.SaveAsFile(filePath);
+
+                }catch(Exception ex)
+                {
+                    TestContext.WriteLine($"[Screenshot Error] {ex.Message}");
+                }
+            }
+            this.driver.Quit();
+        }
+
+    }
+}
